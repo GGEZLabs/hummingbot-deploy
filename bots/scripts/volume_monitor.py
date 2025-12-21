562934f079b17f08837fd8e88c574bd270ec60c2
shaker113 <shaker.baker113@gmail.com>
2025-12-21 13:05:48 +0300

    fixing VolumeMonitor strategy refresh time

--- bots/scripts/volume_monitor.py
+++ bots/scripts/volume_monitor.py
@@ -3,18 +3,23 @@ import os
 from decimal import Decimal
 from typing import Dict, List
 
+from pydantic import Field, field_validator
+
 from hummingbot.client.config.config_data_types import BaseClientModel
 from hummingbot.connector.connector_base import ConnectorBase
 from hummingbot.core.data_type.common import PriceType
 from hummingbot.core.utils.async_utils import safe_ensure_future
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
-from pydantic import Field, field_validator
 
 
 class VolumeMonitorConfig(BaseClientModel):
     script_file_name: str = os.path.basename(__file__)
     trading_pair: str = Field(
-        "GGEZ1-USDT", json_schema_extra={"prompt": lambda mi: "trading pair to monitor", "prompt_on_new": True}
+        "GGEZ1-USDT",
+        json_schema_extra={
+            "prompt": lambda mi: "trading pair to monitor",
+            "prompt_on_new": True,
+        },
     )
     exchanges: List[str] = Field(
         ["p2b", "coinstore", "uzx"],
@@ -23,9 +28,19 @@ class VolumeMonitorConfig(BaseClientModel):
             "prompt_on_new": True,
         },
     )
-    refresh_time: int = Field(300, json_schema_extra={"prompt": lambda mi: "refresh time in seconds", "prompt_on_new": True})
+    refresh_time: int = Field(
+        300,
+        json_schema_extra={
+            "prompt": lambda mi: "refresh time in seconds",
+            "prompt_on_new": True,
+        },
+    )
     volume_threshold: Decimal = Field(
-        50000, json_schema_extra={"prompt": lambda mi: "volume threshold in (quote)", "prompt_on_new": True}
+        50000,
+        json_schema_extra={
+            "prompt": lambda mi: "volume threshold in (quote)",
+            "prompt_on_new": True,
+        },
     )
 
     @field_validator("exchanges", mode="before")
@@ -59,7 +74,9 @@ class VolumeMonitor(ScriptStrategyBase):
         cls.markets = {exchange: {config.trading_pair} for exchange in config.exchanges}
         cls.price_source = PriceType.MidPrice
 
-    def __init__(self, connectors: Dict[str, ConnectorBase], config: VolumeMonitorConfig):
+    def __init__(
+        self, connectors: Dict[str, ConnectorBase], config: VolumeMonitorConfig
+    ):
         super().__init__(connectors)
         self.config = config
         self._task = None
@@ -74,12 +91,16 @@ class VolumeMonitor(ScriptStrategyBase):
 
     async def check_volume(self):
         for exchange in self.config.exchanges:
-            volume = await self.connectors[exchange].get_volume(self.config.trading_pair)
+            volume = await self.connectors[exchange].get_volume(
+                self.config.trading_pair
+            )
             self.last_volumes[exchange] = volume
             if volume < self.config.volume_threshold:
-                self.logger().notify(f"\n⚠️Warning⚠️:\nVolume is below the threshold ({volume}) on {exchange}")
+                self.logger().notify(
+                    f"\n⚠️Warning⚠️:\nVolume is below the threshold ({volume}) on {exchange}"
+                )
 
-            await asyncio.sleep(self.config.refresh_time)
+        await asyncio.sleep(self.config.refresh_time)
 
     def format_status(self) -> str:
         text = ""
@@ -89,7 +110,6 @@ class VolumeMonitor(ScriptStrategyBase):
             if exchange not in self.last_volumes:
                 continue
             current_volumes += f"\n{exchange}: {self.last_volumes[exchange]} {self.config.trading_pair.split('-')[1]}"
-            current_prices += f"\n{exchange}: {self.connectors[exchange].get_mid_price(self.config.trading_pair)} {
-                                            self.config.trading_pair.split('-')[0]}"
+            current_prices += f"\n{exchange}: {self.connectors[exchange].get_mid_price(self.config.trading_pair)} {self.config.trading_pair.split('-')[0]}"
 
         return text + f"\n\n{current_volumes}\n\n{current_prices}"
